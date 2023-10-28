f2501f6caf591b39e3965b0a910531a8ccf36086
gungun005 <g053780@gmail.com>
2023-10-28 21:01:13 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-//app.use('/', require('./routes/serverRoutes'));
+app.use('/', require('./routes/serverRoutes'));
 app.listen(process.env.PORT || 8100, ()=>{console.log("Listening to port 8000")});
