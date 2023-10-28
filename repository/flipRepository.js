8243ad61a926dcb536268074213c48e94e1b1aa7
gungun005 <g053780@gmail.com>
2023-10-28 21:04:28 +0530

    added Repository

---
+++ repository/flipRepository.js
@@ -0,0 +1,45 @@
+const getFlipUsers = (body) => {
+    return("hey i m there working!");
+ };
+ 
+ const postFlipUsers = (body) => {
+    return("hey i m there working!");
+ };
+ const getFlipUsersRegister=(req,res)=>
+ {
+ return("Hi ! Im registering the users");
+ };
+ const postFlipUsersRegister=(body)=>
+ {
+     return ("Hi I m doing ammendments in your registration");
+ };
+ 
+ const getFlipUsersDelete=(req,res)=>
+ {
+     return("Hi i m the deleted user");
+ };
+ 
+ const postFlipUsersDelete=(body)=>
+ {
+     return("Hi if u want to do any changes in the deleted users");
+ };
+ const getFlipUsersUpdate=(req,res)=>
+ {
+     return("hi if any updates i m their to tackle!");
+ };
+ const postFlipUsersUpdate=(body)=>
+ {
+ return("updating any one of them!");
+ };
+ 
+ 
+ module.exports={
+ getFlipUsers,
+ postFlipUsers,
+ getFlipUsersRegister,
+ postFlipUsersRegister,
+ getFlipUsersDelete,
+ postFlipUsersDelete,
+ getFlipUsersUpdate,
+ postFlipUsersUpdate
+ };
