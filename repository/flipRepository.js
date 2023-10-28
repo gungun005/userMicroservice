c9a5ea3db58d8a5c6b0625e87f5b0a63eed065e6
gungun005 <g053780@gmail.com>
2023-10-29 00:09:44 +0530

    updated repository

--- repository/flipRepository.js
+++ repository/flipRepository.js
@@ -1,37 +1,27 @@
 const getFlipUsers = (body) => {
+    console.log(body);
     return("hey i m there working!");
  };
  
  const postFlipUsers = (body) => {
     return("hey i m there working!");
  };
- const getFlipUsersRegister=(req,res)=>
- {
- return("Hi ! Im registering the users");
+
+ const getFlipUsersRegister=(body)=> {
+    return("Hi ! Im registering the users");
  };
- const postFlipUsersRegister=(body)=>
- {
-     return ("Hi I m doing ammendments in your registration");
- };
- 
- const getFlipUsersDelete=(req,res)=>
- {
-     return("Hi i m the deleted user");
+
+ const postFlipUsersRegister=(body)=>{
+    return ("Hi I m doing ammendments in your registration");
  };
  
- const postFlipUsersDelete=(body)=>
- {
-     return("Hi if u want to do any changes in the deleted users");
+ const getFlipUsersDelete=(body)=>{
+    return("Hi i m the deleted user");
  };
- const getFlipUsersUpdate=(req,res)=>
- {
-     return("hi if any updates i m their to tackle!");
+ const updateFlipUsersUpdate=(body)=>{
+    return("hi if any updates i m their to tackle!");
  };
- const postFlipUsersUpdate=(body)=>
- {
- return("updating any one of them!");
- };
- 
+
  
  module.exports={
  getFlipUsers,
@@ -39,7 +29,5 @@ const getFlipUsers = (body) => {
  getFlipUsersRegister,
  postFlipUsersRegister,
  getFlipUsersDelete,
- postFlipUsersDelete,
- getFlipUsersUpdate,
- postFlipUsersUpdate
+ updateFlipUsersUpdate
  };
