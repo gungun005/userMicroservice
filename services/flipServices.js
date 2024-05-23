622ac48ca6a059ecdc3a767dfbf8b9f6049c4307
gungun005 <g053780@gmail.com>
2024-05-24 02:00:04 +0530

    services added

--- services/flipServices.js
+++ services/flipServices.js
@@ -1,46 +1,38 @@
-
-const flipRepository=require('../repository/flipRepository');
+const flipRepository = require("../repository/flipRepository");
 
 const getFlipUsers = (body) => {
   let getFlipUsers = flipRepository.getFlipUsers(body);
-  return( getFlipUsers);
+  return getFlipUsers;
 };
 const postFlipUsers = (body) => {
-    let postFlipUsers=flipRepository.postFlipUsers(body);
-    return( postFlipUsers);
-  };
-
-const getFlipUsersRegister=(body)=>
-{
- let getFlipUsersRegister=flipRepository.getFlipUsersRegister(body);
- return( getFlipUsersRegister);
-};
-const postFlipUsersRegister=(body)=>
-{
-    let  postFlipUsersRegister=flipRepository.postFlipUsersRegister(body);
-    return(postFlipUsersRegister);
-
+  let postFlipUsers = flipRepository.postFlipUsers(body);
+  return postFlipUsers;
 };
 
-const getFlipUsersDelete=(body)=>
-{
-  let getFlipUsersDelete=flipRepository.getFlipUsersDelete(body);
-  return(getFlipUsersDelete);
+const getFlipUsersRegister = (body) => {
+  let getFlipUsersRegister = flipRepository.getFlipUsersRegister(body);
+  return getFlipUsersRegister;
+};
+const postFlipUsersRegister = (body) => {
+  let postFlipUsersRegister = flipRepository.postFlipUsersRegister(body);
+  return postFlipUsersRegister;
 };
 
-const updateFlipUsersUpdate=(body)=>
-{
-   let updateFlipUsersUpdate=flipRepository.updateFlipUsersUpdate(body);
-   return(updateFlipUsersUpdate);
+const getFlipUsersDelete = (body) => {
+  let getFlipUsersDelete = flipRepository.getFlipUsersDelete(body);
+  return getFlipUsersDelete;
 };
 
+const updateFlipUsersUpdate = (body) => {
+  let updateFlipUsersUpdate = flipRepository.updateFlipUsersUpdate(body);
+  return updateFlipUsersUpdate;
+};
 
-
-module.exports={
-getFlipUsers,
-postFlipUsers,
-getFlipUsersRegister,
-postFlipUsersRegister,
-getFlipUsersDelete,
-updateFlipUsersUpdate
+module.exports = {
+  getFlipUsers,
+  postFlipUsers,
+  getFlipUsersRegister,
+  postFlipUsersRegister,
+  getFlipUsersDelete,
+  updateFlipUsersUpdate,
 };
